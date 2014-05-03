9faeac1222858e893d20354d04e4d2dfb2b7155e
Kei Sugano <tobasojyo@gmail.com>
2014-05-03 10:25:30 +0900

    Update g.sh

--- g.sh
+++ g.sh
@@ -1,3 +1,34 @@
+TAIL=""
+TBM=""
+case $HOST in
+ "-l")
+   LINK="http://"+$2 
+   LINK=$(echo $LINK  | tr -d '+')
+   open $LINK
+   exit 0
+   ;;
+ "-y")
+  HOST="youtube.com"
+  TAIL="results?search_query="
+  ;;
+ "-v")
+  HOST="google.com"
+  TAIL="search?q="
+  TBM="vid"
+  ;;
+ "-i")
+  HOST="google.com"
+  TAIL="search?q="
+  TBM="isch"
+  ;;
+ *)
+  HOST="google.com"
+  TAIL="search?q="
+  ;;
+esac
+
+echo "HOST:$HOST"
+
 QUERY=$2
 NUMERO=1
 for var in "$@"
